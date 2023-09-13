0da2c03ddeb6d54113ef6bd581c75fb6cde4dbe5
yaoleksa <mryaremchyk@gmail.com>
2023-09-13 22:19:53 +0300

    continue

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const http = require('http');
+const fs = require('fs');
 const port = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
-    res.end('start');
+    if(req.method == 'POST') {
+        req.on('data', data => {
+            ['ascii', 'base64', 'base64url', 'hex', 'latin1', 'ucs2', 'utf8', 'utf16le'].forEach(e => {
+                console.log(`${e} ${Buffer.from(data).toString(e)}`);
+            });
+            console.log(Buffer.from(data).toString());
+        });
+        res.end();
+    }
+    fs.readFile('./index.html', (err, data) => {
+        if(err) {
+            console.error(err.message);
+        } else if(data) {
+            res.write(data, err => {
+                if(err) {
+                    console.error(err.message);
+                }
+                res.end();
+            })
+        }
+    });
 }).listen(port, () => {
     console.log(`http://localhost:${port}`);
 })
