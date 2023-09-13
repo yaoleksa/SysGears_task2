e880ce0d597724da2e10a6de693917cb8fa4e785
yaoleksa <mryaremchyk@gmail.com>
2023-09-13 23:16:39 +0300

    good start

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ const port = process.env.PORT || 3000;
 http.createServer((req, res) => {
     if(req.method == 'POST') {
         req.on('data', data => {
-            ['ascii', 'base64', 'base64url', 'hex', 'latin1', 'ucs2', 'utf8', 'utf16le'].forEach(e => {
-                console.log(`${e} ${Buffer.from(data).toString(e)}`);
-            });
-            console.log(Buffer.from(data).toString());
+            const parsedResponse = decodeURI(Buffer.from(data).toString('utf-8').split('=')[1])
+            .replace(/%3A/g, ':')
+            .replace(/%40/g, '@')
+            .replace(/%2C/g, ',')
+            .replace(/%20/g, ' ')
+            .replace(/\+/g, '');
+            const jsonResponse = JSON.parse(parsedResponse);
+            res.write(JSON.stringify(jsonResponse.data), err => {
+                if(err) {
+                    console.error(err.message);
+                }
+                res.end();
+            })
         });
-        res.end();
     }
     fs.readFile('./index.html', (err, data) => {
         if(err) {
