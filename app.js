899b5eb525a5e6a469922b64a7b3bc205e65f723
yaoleksa <mryaremchyk@gmail.com>
2023-09-14 11:38:04 +0300

    add pure sort feature

--- app.js
+++ app.js
@@ -2,17 +2,32 @@ const http = require('http');
 const fs = require('fs');
 const port = process.env.PORT || 3000;
 
+const comparator = (objArr, key) => {
+    const compareFn = (a, b) => {
+        if(a[key] > b[key]) {
+            return 1;
+        } else if(a[key] < b[key]) {
+            return -1;
+        }
+    }
+    return objArr.sort(compareFn);
+};
+
 http.createServer((req, res) => {
     if(req.method == 'POST') {
         req.on('data', data => {
-            const parsedResponse = decodeURI(Buffer.from(data).toString('utf-8').split('=')[1])
-            .replace(/%3A/g, ':')
-            .replace(/%40/g, '@')
-            .replace(/%2C/g, ',')
-            .replace(/%20/g, ' ')
+            const parsedResponse = decodeURIComponent(Buffer.from(data).toString('utf-8').split('=')[1])
             .replace(/\+/g, '');
             const jsonResponse = JSON.parse(parsedResponse);
-            res.write(JSON.stringify(jsonResponse.data), err => {
+            const userLst = jsonResponse.data;
+            const conds = jsonResponse.condition;
+            const sorted = [];
+            if(conds.sortBy) {
+                sorted.push(...comparator(userLst, conds.sortBy[0]));
+            } else {
+                sorted.push(...userLst);
+            }
+            res.write(JSON.stringify(sorted), err => {
                 if(err) {
                     console.error(err.message);
                 }
